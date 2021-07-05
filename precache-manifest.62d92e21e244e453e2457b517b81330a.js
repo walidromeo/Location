12d501751fa53cb902faa0b3a1e7ffefe2da5e38
walidromeo <walidnes109@gmail.com>
2021-07-05 22:28:36 +0100

    Updates

---
+++ precache-manifest.62d92e21e244e453e2457b517b81330a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964aca4fb161124caabf214c58687a12",
+    "url": "/Location%20/index.html"
+  },
+  {
+    "revision": "975389ea8ee0c6a31c3e",
+    "url": "/Location%20/static/css/main.c320acdb.chunk.css"
+  },
+  {
+    "revision": "0237a2353b0672e0ffb3",
+    "url": "/Location%20/static/js/2.b025bdf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Location%20/static/js/2.b025bdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975389ea8ee0c6a31c3e",
+    "url": "/Location%20/static/js/main.e7def1e5.chunk.js"
+  },
+  {
+    "revision": "578fe87d66991777dcc7",
+    "url": "/Location%20/static/js/runtime-main.811acbc4.js"
+  }
+]);
